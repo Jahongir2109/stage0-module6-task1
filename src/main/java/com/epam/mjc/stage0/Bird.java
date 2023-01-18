939117e7068f18b8c422986f2a1b3534dd5eba8d
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-18 23:25:06 +0500

    changed all some helps

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -6,6 +6,10 @@ public class Bird extends Animal {
         super("blue", 2, false);
     }
 
+    public Bird() {
+        super("blue",2,false);
+    }
+
     @Override
     public String getDescription() {
         return super.getDescription()+" Moreover, it has 2 wings and can fly.";
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -2,7 +2,11 @@ package com.epam.mjc.stage0;
 
 public class Dog extends Animal{
     public Dog(String color, int numberOfPaws, boolean hasFur) {
-        super("brown", 4, false);
+        super("brown", 4, true);
+    }
+
+    public Dog() {
+        super("brown",4,true);
     }
 
     @Override
